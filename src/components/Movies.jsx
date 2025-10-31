5707f0a533dc5ace6b1c42a7eef685c081d3da05
Akash Das <akashdas02052@gmail.com>
2025-10-31 15:23:27 +0530

    second commit

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,7 +1,7 @@
 import useFetch from "../useFetch"
 
 const Movies = () => {
-  const { data, loading, error } = useFetch("http://localhost:3000/movies")
+  const { data, loading, error } = useFetch("http://localhost:3000/getMovies")
   // console.log(data)
 
   return (
